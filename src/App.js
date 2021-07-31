37cbb0158d35a2cd2e9e53c751a7d74ab6880753
rkh-leung <leptanian@gmail.com>
2021-07-31 17:15:15 +1200

    add: DrumMachine component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">FCC Drum Machine</header>
+      <DrumMachine />
     </div>
   );
 }
 
 export default App;
+
+const DrumMachine = () => (
+    <div id={'drum-machine'}>
+
+    </div>
+  )
