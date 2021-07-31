5f77fa84660ce8d90595d85b4684575f945dce04
rkh-leung <leptanian@gmail.com>
2021-07-31 17:45:05 +1200

    add: map to render buttons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">FCC Drum Machine</header>
+    <div className='App'>
+      <header className='App-header'>FCC Drum Machine</header>
       <DrumMachine />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
 
-const DrumMachine = () => (
+const DrumMachine = () => {
+  const arr = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
+  const playMusic = () => {
+    const sound = new Audio()
+  }
+  return (
     <div id={'drum-machine'}>
-
+      <div id={'display'}>
+        {arr.map((item, idx) => (
+          <button key={idx} className={'drum-pad'} onClick={playMusic}>
+            {item}
+          </button>
+        ))}
+      </div>
     </div>
   )
+}
